eff4ea07394dce04eb836b3fffe6f940698eaeb7
breaks23 <breaks23@naver.com>
2024-10-31 17:52:32 +0900

    업로드용

--- SEMI_Project/src/main/java/com/kh/unionBoard/model/dao/UnionBoardDao.java
+++ SEMI_Project/src/main/java/com/kh/unionBoard/model/dao/UnionBoardDao.java
@@ -2,10 +2,13 @@ package com.kh.unionBoard.model.dao;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
 
+import com.kh.common.JDBCTemplate;
 import com.kh.request.model.dao.RequestDao;
 
 public class UnionBoardDao {
@@ -25,4 +28,50 @@ public class UnionBoardDao {
 			e.printStackTrace();
 		}
 	}
+
+	
+	   //총 게시글 개수 조회
+	public int listCount(Connection conn) {
+		
+		 PreparedStatement pstmt = null;
+		 ResultSet rset = null;
+		 
+		 int listCount = 0;
+		 
+		 String sql = pro.getProperty("listCount");
+		 
+		 
+		     try {
+				pstmt = conn.prepareStatement(sql);
+				
+				rset = pstmt.executeQuery();
+				
+				if(rset.next()) {
+					
+					listCount = rset.getInt("COUNT");
+				}
+				
+				
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				
+				JDBCTemplate.close(rset);
+				JDBCTemplate.close(pstmt);
+			}
+		
+		
+		
+		return listCount;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
