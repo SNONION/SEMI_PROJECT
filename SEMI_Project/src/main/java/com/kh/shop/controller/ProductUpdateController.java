44999b88e2e05df63b87a673bde8689db4e49183
jhg08100@naver.com <jhg08100>
2024-11-04 09:40:54 +0900

    11/4 수정내용 업로드

---
+++ SEMI_Project/src/main/java/com/kh/shop/controller/ProductUpdateController.java
@@ -0,0 +1,67 @@
+package com.kh.shop.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.kh.shop.model.service.ShopService;
+import com.kh.shop.model.vo.Product;
+import com.kh.shop.model.vo.ShopMediaFile;
+
+/**
+ * Servlet implementation class ProductUpdateController
+ */
+@WebServlet("/update.sh")
+public class ProductUpdateController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductUpdateController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+				int bno = Integer.parseInt(request.getParameter("bno"));
+				//수정페이지로 보내야하는 데이터
+				//게시글 정보,카테고리 목록, 첨부파일 정보
+				
+				ShopService service = new ShopService();
+				
+				Product p = service.selectProduct(bno);
+				
+				ShopMediaFile smf = service.selectShopMediaFile(bno);
+				
+				//ArrayList<Category> cList = service.selectCategory();
+				
+				request.setAttribute("p", p);
+				request.setAttribute("smf", smf);
+				//request.setAttribute("cList", cList);
+				
+				request.getRequestDispatcher("/views/board/boardUpdateForm.jsp").forward(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
